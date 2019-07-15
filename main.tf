c3a517032f00dc532f2c4a706ec1da446059b8e6
Jim Ray <ray.jim@gmail.com>
2019-07-14 21:29:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "jray-demo-ec2-TF12" {
+resource "aws_instance" "jray-demo-ec2-TF-API" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.small"
 
   tags = {
-    Name  = "jray-demo-ec2-restrict-test"
+    Name  = "jray-demo-ec2-TF-API"
     owner = "jray@hashicorp.com"
     ttl   = "2"
   }
